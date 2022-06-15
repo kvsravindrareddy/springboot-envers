45d27af6d67580d6818fa5cddb6bf24e347c527a
kvsravindrareddy <kvsravindrareddy@gmail.com>
2022-06-15 20:36:11 +0530

    test

---
+++ src/main/java/com/ravindra/Application.java
@@ -0,0 +1,7 @@
+package com.ravindra;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/ravindra/entity/Employee.java
@@ -0,0 +1,7 @@
+package com.ravindra.entity;
+
+public class Employee {
+    private int id;
+    private String name;
+    
+}
