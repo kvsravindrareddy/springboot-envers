cceda6db220d2a7d65ba90272a25ef10da37cce5
kvsravindrareddy <kvsravindrareddy@gmail.com>
2022-06-15 23:56:04 +0530

    cherry pick test

--- src/main/java/com/ravindra/Application.java
+++ src/main/java/com/ravindra/Application.java
@@ -1,10 +1,11 @@
 package com.ravindra;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
