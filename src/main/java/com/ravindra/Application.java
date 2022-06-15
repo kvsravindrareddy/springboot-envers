b83be82d167281a8a53b2b4dac8e00c20bd79c4c
kvsravindrareddy <kvsravindrareddy@gmail.com>
2022-06-15 23:29:46 +0530

    Squash entity changes

--- src/main/java/com/ravindra/Application.java
+++ src/main/java/com/ravindra/Application.java
@@ -1,5 +1,8 @@
 package com.ravindra;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 
